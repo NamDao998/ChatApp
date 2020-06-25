029bf5786098fde93cf2bed5cf9cc652d4d86300
NamDao998 <namdao30598@gmail.com>
2020-06-25 22:08:58 +0700

    123

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 window.onload = () => {
-    // console.log('loaded');
-    view.setActiveScreen('registerScreen')
-    view.setActiveScreen('loginScreen')
-    
-}
+  var firebaseConfig = {
+    apiKey: "AIzaSyDExcAiqNsk2EO0L_M-9eBmDnuET7ocyzk",
+    authDomain: "chat-ci-44-f5876.firebaseapp.com",
+    databaseURL: "https://chat-ci-44-f5876.firebaseio.com",
+    projectId: "chat-ci-44-f5876",
+    storageBucket: "chat-ci-44-f5876.appspot.com",
+    messagingSenderId: "297945081715",
+    appId: "1:297945081715:web:5d2bee18602cced0ca8706",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   console.log(firebase.app().name);
+  view.setActiveScreen("registerScreen");
+  view.setActiveScreen("loginScreen");
+};
--- js/view.js
+++ js/view.js
@@ -1,60 +1,49 @@
 const view = {};
-view.setActiveScreen = (screenName) =>{
-    switch (screenName) {
-        case `registerScreen`:
-            document.getElementById('app').innerHTML = components.registerScreen
-            const registerForm = document.getElementById('form-register')
-            registerForm.addEventListener('submit', (e) =>{
-                e.preventDefault()
-                const registerInfo = {
-                    firstName: registerForm.firstName.value,
-                    lastName: registerForm.lastName.value,
-                    email: registerForm.email.value,
-                    password: registerForm.password.value,
-                    comfirmPassword: registerForm.confirmPassword.value,
-                }
-                controller.register(registerInfo)
-                if (registerInfo.firstName === ''){
-                    document.getElementById('error-first-name').innerText = 'Please input first name'
-                }
-                if (registerInfo.lastName === ''){
-                    document.getElementById('error-last-name').innerText = 'Please input last name'
-                }
-                if 
-                (registerInfo.email === ''){
-                    document.getElementById('error-email-name').innerText = 'Please input email name'
-                }
-                if (registerInfo.password === ''){
-                    document.getElementById('error-password-name').innerText = 'Please input password'
-                }
-                if (registerInfo.confirmPassword === ''){
-                    document.getElementById('error-confirm-password-name').innerText = 'Please input confirm password'
-                }
-                
-            }) 
-            const redirectToLogin = document.getElementById('redirect-to-login');
-            redirectToLogin.addEventListener('click', (e) =>{
-                e.preventDefault()
-                view.setActiveScreen('loginScreen');
-            });
-            break;
-        case `loginScreen`:
-            document.getElementById('app').innerHTML = components.loginScreen
-             const loginForm = document.getElementById('form-login')
-            loginForm.addEventListener('submit', (e) => {
-                e.preventDefault()
-                const loginInfo = {
-                    email : loginForm.email.value,
-                    password : loginForm.password.value
-                }
-                controller.login(loginInfo)
-            })
-            const redirectToRegister = document.getElementById('redirect-to-register')
-            redirectToRegister.addEventListener('click', (e) => {
-                e.preventDefault()
-                view.setActiveScreen('registerScreen')
-            })  
-            break;
-    }
-            
+view.setActiveScreen = (screenName) => {
+  switch (screenName) {
+    case "registerScreen":
+      document.getElementById("app").innerHTML = components.registerScreen;
+      const registerForm = document.getElementById("form-register");
+      registerForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const registerInfo = {
+          firstName: registerForm.firstName.value,
+          lastName: registerForm.lastName.value,
+          email: registerForm.email.value,
+          password: registerForm.password.value,
+          confirmPassword: registerForm.confirmPassword.value,
+        };
+        controller.register(registerInfo);
+      });
+      const redirectToLogin = document.getElementById("redirect-to-login");
+      redirectToLogin.addEventListener("click", (e) => {
+        view.setActiveScreen("loginScreen");
+      });
+      break;
+    case "loginScreen":
+      document.getElementById("app").innerHTML = components.loginScreen;
+      const loginForm = document.getElementById("form-login");
+      loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const loginInfo = {
+          email: loginForm.email.value,
+          password: loginForm.password.value,
+        };
+        controller.login(loginInfo);
+      });
+      document
+        .getElementById("redirect-to-register")
+        .addEventListener("click", (e) => {
+          view.setActiveScreen("registerScreen");
+        });
+         break;
+     case "chatScreen":
+         document.getElementById('app').innerHTML = components.chatScreen;
+      document.getElementById('welcome-user').innerText = 'Welcome ' + model.currentUser.displayName
+          break; 
 }
+  
+};
+view.setErrorMessage = (elementId, message) => {
+  document.getElementById(elementId).innerText = message;
+};
